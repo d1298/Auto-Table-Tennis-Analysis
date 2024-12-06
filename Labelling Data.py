9adc5975501d1e914f699799f77ac0fa005d2147
d1298 <60811901+d1298@users.noreply.github.com>
2024-12-06 17:32:19 +0000

    got equatoion generator

--- Labelling Data.py
+++ Labelling Data.py
@@ -26,26 +26,29 @@ bp,bb,bd,fp,fb,fd = False, False, False, False, False, False
 
 #Where B is the centre angle, aka angle(ABC)
 def get_angle(ax,ay,az,bx,by,bz,cx,cy,cz):
+    ax,ay,az,bx,by,bz,cx,cy,cz = float(ax), float(ay), float(az), float(bx), float(by), float(bz), float(cx), float(cy), float(cz)
+    
+    
     AB = math.sqrt((ax-bx)**2 + (ay-by)**2 + (az-bz)**2)
     BC = math.sqrt((bx-cx)**2 + (by-cy)**2 + (bz-cz)**2)
     AC = math.sqrt((ax-cx)**2 + (ay-cy)**2 + (az-cz)**2)
-    
+        
     #cosine rule 
-    angle = math.degrees(math.acos((AB**2 + BC**2 - AC**2) / (2*AB*BC)))
+    angle = math.degrees(math.acos(float(AB**2 + BC**2 - AC**2) / float(2.0*AB*BC)))
     return angle
 
 def write_data(data):
-    with open("recorded_data/detecting_shots.csv", "a") as file:
-        file.writelines(data)
+    with open("recorded_data/detecting_shots.csv", "w") as file:
+        file.writelines(str(data))
+        print("written data")
+        return
         
         
 def lagrange_interpolation(y_values):
     n = len(y_values)
     x_values = list(range(n))
     
-    
     equation = 0
-    print(y_values, x_values)
     
     for yval in range(n):
         numerator = 1
@@ -59,10 +62,24 @@ def lagrange_interpolation(y_values):
         equation += (numerator/denominator) * y_values[yval]
         
     equation = expand(equation)
+    print(equation)
         
     return equation
 
-def frames_to_equations(shot-index,starting_frame, ending_frame):
+def frames_to_equations(shot,starting_frame, ending_frame):
+    
+    shots = {
+        0: "None",
+        1: "Backhand Push",
+        2: "Backhand Block",
+        3: "Backhand Drive",
+        4: "Forehand Push",
+        5: "Forehand Block",
+        6: "Forehand Drive"
+    }
+    
+    print(shots[shot] + " ended")
+    
     with open ("recorded_data/test1.csv", "r") as file:
         datatemp = file.read().splitlines()
         data = []
@@ -76,22 +93,23 @@ def frames_to_equations(shot-index,starting_frame, ending_frame):
             frame_index = starting_frame + i
 
             opts = [[1,2,3],[4,5,6],[7,8,9],[10,11,12],[13,14,15],[16,17,18],[19,20,21],[22,23,24],[25,26,27],[28,29,30],[31,32,33],[34,35,36]]
+            
             for a in range(12):
                 for b in range(12):
                     for c in range(12):
-                        angles[i].append(get_angle(data[frame_index][opts[a][0]],data[frame_index][opts[a][1]],data[frame_index][opts[a][2]],data[frame_index][opts[b][0]],data[frame_index][opts[b][1]],data[frame_index][opts[b][2]],data[frame_index][opts[c][0]],data[frame_index][opts[c][1]],data[frame_index][opts[c][2]]))
+                        if a != b and b != c and a != c:
+                            angles[i].append(get_angle(data[frame_index][(opts[a])[0]],data[frame_index][(opts[a])[1]],data[frame_index][(opts[a])[2]],data[frame_index][(opts[b])[0]],data[frame_index][(opts[b])[1]],data[frame_index][(opts[b])[2]],data[frame_index][(opts[c])[0]],data[frame_index][(opts[c])[1]],data[frame_index][(opts[c])[2]]))
             
-        equations = [starting_frame, ending_frame]
+        equations = [shot,starting_frame, ending_frame]
                     
         for i in range(len(angles[0])):
             anglestoequation = []
             for b in range(len(angles)):
                 anglestoequation.append(angles[b][i])
             equations.append(lagrange_interpolation(anglestoequation))
+            
         write_data(equations)
-        print("written data")
-
-
+        return
 
 #Probably use polynomial interpolation (newton) to approximate the equation, offset all frame numbers so the first one is zero to simplify the whole thing
 
@@ -101,7 +119,12 @@ def frames_to_equations(shot-index,starting_frame, ending_frame):
 #frame number, LSx,LSy,LSz,  RSx,RSy,RSz,  LEx,LEy,LEz,  REx,REy,REz,  LWx,LWy,LWz,  RWx,RWy,RWz,  LHx,LHy,LHz,  RHx,RHy,RHz,  LKx,LKy,LKz,  RKx,RKy,RKz,  LAx,LAy,LAz,  RAx,RAy,RAz
 #   0            1  2   3     4   5   6     7   8   9     10  11  12   13  14  15    16  17  18     19  20  21    22  23  24    25  26  27    28  29  30    31  32  33    34  35  36
 
+def end_recording( shot, frame_number, starting_frame):
+    
+    frames_to_equations(shot,starting_frame,frame_number)
+    time.sleep(0.1)
 
+    return
 
 while cap.isOpened():
     read, frame = cap.read()
@@ -112,77 +135,28 @@ while cap.isOpened():
     
     cv2.imshow("Video", frame)
     
-    #need to simplify this --------------------------
-    if keyboard.is_pressed("1"): 
-        if bp == False:
-            print("Backhand Push Started", frame_number)
-            starting_frame = frame_number
-            bp = True
-        elif bp == True:
-            print("Backhand Push Ended",frame_number)
-            ending_frame = frame_number
-            frames_to_equations(0,starting_frame,ending_frame)
-            bp = False
+    if keyboard.is_pressed(" "): 
+        starting_frame = frame_number
+        time.sleep(0.1)
     
-    if keyboard.is_pressed("2"): 
-        if bb == False:
-            print("Backhand Block Started", frame_number)
-            starting_frame = frame_number
-            bb = True
-        if bb == True:
-            print("Backhand Block Ended",frame_number)
-            ending_frame = frame_number
-            frames_to_equations(1,starting_frame,ending_frame)
-            bb = False        
-            
-    if keyboard.is_pressed("3"): 
-        if bd == False:
-            print("Backhand Drive Started", frame_number)
-            starting_frame = frame_number
-            bd = True
-        if bd == True:
-            print("Backhand Drive Ended",frame_number)
-            ending_frame = frame_number
-            frames_to_equations(2,starting_frame,ending_frame)
-            bd = False
+    elif keyboard.is_pressed("1"): 
+        end_recording(1, frame_number, starting_frame)
     
-    if keyboard.is_pressed("4"): 
-        if fp == False:
-            print("Forehand Push Started", frame_number)
-            starting_frame = frame_number
-            fp = True
-        if fp == True:
-            print("Forehand Push Ended",frame_number)
-            ending_frame = frame_number
-            frames_to_equations(3,starting_frame,ending_frame)
-            fp = False
-                
-    if keyboard.is_pressed("5"): 
-        if fb == False:
-            print("Forehand Block Started", frame_number)
-            starting_frame = frame_number
-            fb = True
-        if fb == True:
-            print("Forehand Block Ended",frame_number)
-            ending_frame = frame_number
-            frames_to_equations(4,starting_frame,ending_frame)
-            fb = False       
-                
-    if keyboard.is_pressed("6"): 
-        if fd == False:
-            print("Forehand Drive Started", frame_number)
-            starting_frame = frame_number
-            fd = True
-        if fd == True:
-            print("Forehand Drive Ended",frame_number)
-            ending_frame = frame_number
-            frames_to_equations(5,starting_frame,ending_frame)
-            fd = False               
-    # ------------------------------------------------------------------------------
-    
-    
+    elif keyboard.is_pressed("2"): 
+        end_recording(2, frame_number, starting_frame)
         
-            
+    elif keyboard.is_pressed("3"): 
+        end_recording(3, frame_number, starting_frame)
+
+    elif keyboard.is_pressed("4"): 
+        end_recording(4, frame_number, starting_frame)
+      
+    elif keyboard.is_pressed("5"): 
+        end_recording(5, frame_number, starting_frame)
+    
+    elif keyboard.is_pressed("6"): 
+        end_recording(6, frame_number, starting_frame)
+    
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
     
